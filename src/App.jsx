0ad1fffea672d90216e73173569a46a46b2b6b88
Aiswarya <AiswaryaMenon14>
2025-04-06 02:27:50 +0530

    Revert "Changes"
    This reverts commit 6f5c59f69e0e6bf9177b6189d866fdbf94ef27c1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Counter from "./components/Counter";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +12,6 @@ function App() {
     <>
       <Header />
       <Counter />
-      <Footer />
     </>
   );
 }
